015eb09c000f03f89de4f70247791ec4b989bdd2
Juan Batiz-Benet <juan@benet.ai>
2015-08-17 05:56:07 +0200

    fix markdown rendering

--- gen-index.js
+++ gen-index.js
@@ -1,5 +1,10 @@
-var njx = require('njx')
 var fs = require('fs')
+var marked = require('marked')
+var nunjucks = require('nunjucks')
+var njmd = require('nunjucks-markdown')
+
+var njx = nunjucks.configure('.')
+njmd.register(njx, marked)
 
 var spec = {
   template: "tmpl/index.html",
@@ -9,7 +14,7 @@ var spec = {
 var goipfsPath = './releases/go-ipfs/v0.3.7/dist.json'
 spec.data.dists['go-ipfs'] = require(goipfsPath)
 
-njx.render(spec, function(err, res) {
+njx.render(spec.template, spec.data, function(err, res) {
   if (err) throw err
   process.stdout.write(res)
   process.exit(0)
