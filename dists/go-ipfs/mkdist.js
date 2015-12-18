2ddcdc978d6610a46b19d475753ce2eeb85d48fe
dignifiedquire <dignifiedquire@gmail.com>
2015-12-18 15:45:30 +0100

    Links and dist.json cleanup for go-ipfs

--- dists/go-ipfs/mkdist.js
+++ dists/go-ipfs/mkdist.js
@@ -1,7 +1,7 @@
 var mkdist = require('../../scripts/mkdist')
 
 if (process.argv.length != 3) {
-  console.log("usage: mkdist.js <version>")
+  console.log('usage: mkdist.js <version>')
   process.exit(1)
 }
 
@@ -10,19 +10,19 @@ dist.version = process.argv[2]
 
 mkdist({
   dist: dist,
-  path: "../..",
+  path: '../..',
   platforms: {
     oses: [
-      {id: 'darwin',  name: "Mac OSX App", browser:'OS X',    icon: 'apple'},
-      {id: 'linux',   name: "Linux App",   browser:'Linux',   icon: 'linux'},
-      {id: 'openbsd', name: "OpenBSD App", browser:'OpenBSD', icon: 'circle-o'},
-      {id: 'freebsd', name: "FreeBSD App", browser:'FreeBSD', icon: 'circle-o'},
-      {id: 'windows', name: "Windows App", browser:'Windows', icon: 'windows'},
+      {id: 'darwin',  name: 'Mac OSX Binary (.zip)', browser: 'OS X'},
+      {id: 'linux',   name: 'Linux Binary (.zip)',   browser: 'Linux'},
+      {id: 'openbsd', name: 'OpenBSD Binary (.zip)', browser: 'OpenBSD'},
+      {id: 'freebsd', name: 'FreeBSD Binary (.zip)', browser: 'FreeBSD'},
+      {id: 'windows', name: 'Windows Binary (.zip)', browser: 'Windows'}
     ],
     archs: [
       {id: '386',   name: '32 bit', browser: '32'},
       {id: 'amd64', name: '64 bit', browser: '64'},
-      {id: 'arm',   name: 'ARM',    browser: 'ARM'},
+      {id: 'arm',   name: 'ARM',    browser: 'ARM'}
     ]
   }
 })
