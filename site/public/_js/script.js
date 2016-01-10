a7c59a996d0c522c59972790462f288c72a795bf
dignifiedquire <dignifiedquire@gmail.com>
2016-01-10 23:59:42 +0100

    Make usable on mobile

--- site/public/_js/script.js
+++ site/public/_js/script.js
@@ -2,5 +2,6 @@ const $ = window.jQuery = require('jquery')
 window.Tether = require('tether')
 const Stickyfill = require('stickyfill')()
 require('bootstrap')
+require('bootstrap-offcanvas/dist/js/bootstrap.offcanvas.js')
 
 Stickyfill.add($('.sticky')[0])
