bb1c5a5b39f45b9010fcad7ae526ec0ff24fabb1
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-02-21 20:08:50 +0100

    feat: add dependency checks for site builds

--- Makefile
+++ Makefile
@@ -2,18 +2,21 @@ all: releases all_dists site
 
 dists_to_build=$(shell ls dists)
 
-.PHONY: all all_dists
+.PHONY: all all_dists deps
 all_dists: $(dists_to_build)
 
 %:
 	echo "** $@ **"
 	cd dists/$@ && make
 
+deps:
+	./deps-check.sh
+
 releases:
 	mkdir -p releases
 
 .PHONY: site
-site:
+site: deps
 	echo "** Building site **"
 	npm run build
 
---
+++ deps-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function fail() {
+	printf $txtred%s$txtnon\\n "$@"
+	exit 1
+}
+
+(hugo version > /dev/null 2>&1) || (fail "Missing hugo, please see https://gohugo.io/")
+
+(node --version > /dev/null 2>&1) || (fail "Missing node.js, please see https://nodejs.org/")
+
+(npm --version > /dev/null 2>&1) || (fail "Missing npm, please see https://npmjs.com/")
+
+(yarn --version > /dev/null 2>&1) || (fail "Missing yarn, please install with `npm i -g yarn`")
+
+exec yarn
