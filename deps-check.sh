0867947e2a9ca6110efd3f187e0e541e48166134
Steven Allen <steven@stebalien.com>
2021-04-15 09:11:02 -0700

    remove perl dependency
    On linux, sha512sum is a part of the "coreutils" package. shasum is a
    part of perl.

--- deps-check.sh
+++ deps-check.sh
@@ -14,7 +14,8 @@ function fail() {
 (ipfs version > /dev/null 2>&1) || fail "Missing ipfs, please install it :-)"
 (ipfs swarm peers > /dev/null 2>&1) || fail "ipfs is offline, please run ipfs daemon"
 (awk --version > /dev/null 2>&1) || fail "Missing awk, please install it"
-(date | shasum -a 512 - > /dev/null 2>&1) || fail "Missing shasum with sha512 support, please install it"
+(date | shasum -a 512 - > /dev/null 2>&1) ||
+    (date | sha512sum - > /dev/null 2>&1) || fail "Missing shasum with sha512 support, please install it"
 (node --version > /dev/null 2>&1) || fail "Missing node.js, please see https://nodejs.org/"
 (npm --version > /dev/null 2>&1) || fail "Missing npm, please see https://npmjs.com/"
 (jq --version > /dev/null 2>&1) || fail "Missing jq, please see https://stedolan.github.io/jq/"
