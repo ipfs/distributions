c0c25a8be276e17d8001db3391ec2df4e62aaa23
thattommyhall <thattommyhall@gmail.com>
2021-07-22 14:55:24 +0100

    Make xml use dist update times as lastBuildDate

--- scripts/dist.js
+++ scripts/dist.js
@@ -16,7 +16,7 @@ const IpfsHttpClient = require('ipfs-http-client')
 require('make-promises-safe') // exit on error
 
 const RELEASE_PATH = join(__dirname, '..', 'releases')
-const SITE_PATH = join(__dirname, '..', 'site', 'data', 'releases')
+const HUGO_DATA_DIR = join(__dirname, '..', 'site', 'data')
 const DIST_PATH = join(__dirname, '..', 'dists')
 const DIST_ROOT = '/ipns/dist.ipfs.io'
 
@@ -48,10 +48,14 @@ async function fetchDistData (distName) {
   return data
 }
 
-async function writeToHugoDataDir (distName, data) {
-  const dataTargetPath = join(SITE_PATH, distName)
+async function writeToRelease (distName, data) {
+  const dataTargetPath = join(HUGO_DATA_DIR, 'releases', distName)
   await fs.promises.mkdir(dataTargetPath, { recursive: true })
-  return fs.promises.writeFile(join(dataTargetPath, 'data.json'), JSON.stringify(data, null, 2))
+  return writeFile(join(dataTargetPath, 'data.json'), data)
+}
+
+function writeFile (path, data) {
+  return fs.promises.writeFile(path, JSON.stringify(data, null, 2))
 }
 
 async function updateHugoDataFiles () {
@@ -66,6 +70,7 @@ async function updateHugoDataFiles () {
   const items = await fs.promises.readdir(DIST_PATH)
   console.log(`Updating hugo data for dist website`)
 
+  var latest = new Date(0)
   for (const distName of items) {
     const noSite = join(DIST_PATH, distName, 'no-site')
     if (fs.existsSync(noSite)) {
@@ -74,8 +79,15 @@ async function updateHugoDataFiles () {
     }
     console.log(`- Generating site for ${distName}`)
     const data = await fetchDistData(distName)
-    await writeToHugoDataDir(distName, data)
+    const date = Date.parse(data.dateUTC)
+    if (date > latest) {
+      latest = date
+    }
+    await writeToRelease(distName, data)
   }
+  const siteRootJSON = join(HUGO_DATA_DIR, 'siteroot.json')
+  const utcDate = new Date(latest).toUTCString()
+  await writeFile(siteRootJSON, { lastBuildDate: utcDate })
 }
 
 updateHugoDataFiles()
