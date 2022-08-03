b51659077e02b163db5f9846b2b2c04311c1bdf2
Marcin Rataj <lidel@lidel.org>
2022-08-03 21:36:14 +0200

    feat: redirect .io to .tech

--- site/assets/js/index.js
+++ site/assets/js/index.js
@@ -4,8 +4,19 @@ require('bootstrap/dist/js/bootstrap.js')
 require('slicknav/dist/jquery.slicknav.js')
 
 const handlePlatform = require('./platform')
+const maybeRedirectToNewDomain = () => {
+  // https://github.com/protocol/bifrost-infra/issues/2018#issue-1319432302
+  const { href } = window.location
+  if (href.includes('dist.ipfs.io')) {
+    window.location.replace(href.replace('dist.ipfs.io', 'dist.ipfs.tech'))
+  }
+  if (href.includes('dist-ipfs-io')) {
+    window.location.replace(href.replace('dist-ipfs-io', 'dist-ipfs-tech'))
+  }
+}
 
 $(() => {
+  maybeRedirectToNewDomain()
   handlePlatform()
 
   $('#d-navbar').slicknav({
