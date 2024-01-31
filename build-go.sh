a2c27320d86f07ab9b3813d553d1e50ca3df39a8
Marcin Rataj <lidel@lidel.org>
2024-01-31 15:36:39 +0100

    test(ci): trigger re-build of recent release

--- build-go.sh
+++ build-go.sh
@@ -107,7 +107,7 @@ function goBuild() {
 		local output
 		output="$(pwd)/$(basename "$package")$(go env GOEXE)"
 		if ! (go build -mod=mod -o "$output" -trimpath "${package}"); then
-			warn "    go build with $package $goos $goarch failed."
+			warn "    go build of $output failed."
 			return 1
 		fi
 
@@ -145,7 +145,7 @@ function doBuild() {
 
 	mkdir -p "$dir"
 
-	if ! (cd "$build_dir_name" && goBuild "$package" "$goos" "$goarch") > build-log; then
+	if ! (cd "$build_dir_name" && goBuild "$package" "$goos" "$goarch") > "$build_dir_name/build-log" 2>&1; then
 		local logfi="$dir/build-log-$goos-$goarch"
 		cp "$build_dir_name/build-log" "$logfi"
 		warn "    $binname failed. logfile at '$logfi'"
