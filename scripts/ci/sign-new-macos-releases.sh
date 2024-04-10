8c700ab53298a9ed0e47f19b6277b7fca443e62e
Marcin Rataj <lidel@lidel.org>
2024-04-10 16:55:30 +0200

    fix(ci): switch osx build to codesign and notarytool (#1078)
    See details in https://github.com/ipfs/distributions/pull/1078
    Closes #1066

--- scripts/ci/sign-new-macos-releases.sh
+++ scripts/ci/sign-new-macos-releases.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+echo "::group::Store credentials to avoid GUI prompt in CI"
+    xcrun notarytool store-credentials "notarytool-profile" \
+        --apple-id "${APPLE_AC_USERNAME}" --team-id "${APPLE_AC_TEAM_ID}" --password "${APPLE_AC_PASSWORD}"
+echo "::endgroup::"
+
 echo "::group::Unpack any new darwin arm64 and amd64 binaries to ./tmp"
     # ./releases/{DIST_NAME}/{DIST_VERSION}/*_darwin-${arch}.tar.gz
     # -> ./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-unsigned/
@@ -18,7 +23,7 @@ echo "::group::Unpack any new darwin arm64 and amd64 binaries to ./tmp"
     ls -Rhl ./tmp || echo "Nothing new in ./tmp"
 echo "::endgroup::"
 
-echo "::group::Sign and notarize the mac binaries"
+echo "::group::Unpack .zip and sign the binaries"
     # Find and sign executables in
     # ./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-unsigned/
     for NEW_DIR in ./releases/*/*; do
@@ -27,22 +32,60 @@ echo "::group::Sign and notarize the mac binaries"
         DIST_NAME=$(basename $(dirname "$NEW_DIR"))
         DIST_MAC_ARCHS=$(gawk '{ print $2; }' <(grep darwin "./dists/${DIST_NAME}/build_matrix"))
         for arch in $DIST_MAC_ARCHS; do
-            EXECUTABLES=$(jq -nc '$ARGS.positional' --args $(find "./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-unsigned/" -perm +111 -type f -print))
-            echo "-> Signing ${EXECUTABLES}"
-            echo "{
-                \"source\" : $EXECUTABLES,
-                \"bundle_id\" : \"io.ipfs.dist.${DIST_NAME}\",
-                \"apple_id\": {
-                \"password\":  \"@env:AC_PASSWORD\"
-                },
-                \"sign\" :{
-                \"application_identity\" : \"Developer ID Application: Protocol Labs, Inc. (7Y229E2YRL)\"
-                },
-                \"zip\" :{
-                    \"output_path\" : \"./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-signed.zip\"
-                }
-            }" | tee | jq > "./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-gon.json"
-            gon -log-level=info -log-json "./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-gon.json"
+            # create destination dir matching .tar.gz structure
+            mkdir -p "${WORK_DIR}/tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-signed/${DIST_NAME}"
+            # find executable files, and process each one
+            find "${WORK_DIR}/tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-unsigned" -perm +111 -type f -print | while read -r file; do
+                # -perm +111 will return all executables, including .sh scripts
+                # so we need to skip them
+                if [[ "$file" == *.sh ]]; then
+                    echo "-- Skipping shell script ${file}"
+                    continue
+                fi
+
+                echo "-> Processing ${file}"
+                ls -hl "${file}"
+
+                echo "-> Signing ${file}"
+
+                # Sign with Apple's tool
+                # All credentials are imported to macOS keychain
+                # and will be found via TEAM_ID match
+                xcrun codesign --force --verbose --display --timestamp --options=runtime --sign "$APPLE_AC_TEAM_ID" "${file}"
+
+                # TODO: revisit switch to rcodesign once we have to generate new credentials anyway
+                # if we use rcodesign if we ever swithc away from macos runner
+                #rcodesign sign \
+                #    --p12-file ~/.apple-certs.p12 --p12-password-file ~/.apple-certs.pass \
+                #    --code-signature-flags runtime --for-notarization \
+                #    "${file}"
+
+                echo "-> Notarizing ${file}"
+                # The tool (or Apple API) seems to only accept.zip, even if it is a single binary
+                TMP_ZIP=$(mktemp -u -t "${DIST_NAME}_${DIST_VERSION}_${arch}-signed-for-notarization.zip")
+                zip "${TMP_ZIP}" "${file}"
+
+                # Notarize with Apple's notarytool for now (only reason we use macOS runner)
+                xcrun notarytool submit --progress --keychain-profile "notarytool-profile" --wait "${TMP_ZIP}"
+
+                # NOTE: no stappling, because it would break signatures of Mach-O Binaries (which we publish without any .app or .dmg envelope)
+                # This means out binaries will rely on online notarization the first time macOS Gatewkeeper sees a new binary.
+
+                # Verify produced blob is a-ok
+                if ! xcrun spctl --assess --type install --context context:primary-signature --ignore-cache --verbose=2 "${file}"; then
+                    echo "error: Signature of ${file} will not be accepted by Apple Gatekeeper!" 1>&2
+                    exit 1
+                fi
+                #
+                # TODO: revisit switching notarization to rcodesign once we have to generate new credentials anyway
+                # (rcodesign uses "api key" thing which is 3 things, and codesigns appleid + app-specific password
+                # and it was easier to use notarytool on macOS worker than to make rcodesign work)
+                # rcodesign notary-submit --api-key-path ~/.apple-api-key --wait "${file}"
+
+
+                # move signed binaries to a directory matching .tar.gz structure
+                mv "${file}" "${WORK_DIR}/tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-signed/${DIST_NAME}/"
+            done
         done
     done
 echo "::endgroup::"
@@ -59,13 +102,10 @@ echo "::group::Update changed binaries in ./releases"
         DIST_MAC_ARCHS=$(gawk '{ print $2; }' <(grep darwin "./dists/${DIST_NAME}/build_matrix"))
         for arch in $DIST_MAC_ARCHS; do
             echo "-> Starting the update of darwin_${arch}.tar.gz for name='${DIST_NAME}' and version='${DIST_VERSION}'"
-            # unzip signed binaries to a directory matching .tar.gz structure
             cd "${WORK_DIR}"
             mkdir -p "./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-signed/${DIST_NAME}"
             cd "./tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-signed/${DIST_NAME}/"
-            echo "-> Unpacking gon .zip for ${arch}"
-            unzip "${WORK_DIR}/tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-signed.zip"
-            echo "-> Unpacked contents"
+            echo "-> Signed contents"
             ls -Rhl "${WORK_DIR}/tmp/${DIST_NAME}_${DIST_VERSION}_${arch}-signed/"
             # replace .tar.gz with one that has the same structure, but signed binaries
             PKG_NAME="${DIST_NAME}_${DIST_VERSION}_darwin-${arch}.tar.gz"
